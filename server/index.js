2b7da463adced2b3121844a8fa030731079c16b4
Pratima <Pratimasah@gmail.com>
2025-08-17 16:28:45 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const adminRoute = require("./router/admin-router");
 //Middleware
 // app.use(cors());
 app.use(cors({
-  origin: "https://hostelport.netlify.app",
+  origin: ["http://localhost:3000", "https://hostelport.netlify.app"],
   methods: ["GET","POST","PUT","DELETE"],
   credentials: true
 }));
